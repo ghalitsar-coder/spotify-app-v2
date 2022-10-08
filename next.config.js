53fa8f54672581f62f67086f804ba0b72192e144
ghalitsar coder <ghalitsardev@gmail.com>
2022-10-08 15:19:19 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   // env: {
   //   NEXT_SHAZAM_CORE_RAPID_API_KEY: process.env.NEXT_SHAZAM_CORE_RAPID_API_KEY,
   //   NEXT_GEO_API_KEY: process.env.NEXT_GEO_API_KEY,
   // },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
